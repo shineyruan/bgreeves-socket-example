435387ae176ca70f9e64a8a5e789e4dd644f00b9
Zhihao Ruan <ruanzh@umich.edu>
2020-04-27 01:38:30 -0400

    reformat

--- solution/helpers.h
+++ solution/helpers.h
@@ -1,10 +1,10 @@
-#include <arpa/inet.h>		// htons(), ntohs()
-#include <netdb.h>		// gethostbyname(), struct hostent
-#include <netinet/in.h>		// struct sockaddr_in
-#include <stdio.h>		// perror(), fprintf()
-#include <string.h>		// memcpy()
-#include <sys/socket.h>		// getsockname()
-#include <unistd.h>		// stderr
+#include <arpa/inet.h>   // htons(), ntohs()
+#include <netdb.h>       // gethostbyname(), struct hostent
+#include <netinet/in.h>  // struct sockaddr_in
+#include <stdio.h>       // perror(), fprintf()
+#include <string.h>      // memcpy()
+#include <sys/socket.h>  // getsockname()
+#include <unistd.h>      // stderr
 
 /**
  * Make a server sockaddr given a port.
@@ -18,21 +18,21 @@
  *		int err = make_server_sockaddr(&server, 8888);
  */
 int make_server_sockaddr(struct sockaddr_in *addr, int port) {
-	// Step (1): specify socket family.
-	// This is an internet socket.
-	addr->sin_family = AF_INET;
+    // Step (1): specify socket family.
+    // This is an internet socket.
+    addr->sin_family = AF_INET;
 
-	// Step (2): specify socket address (hostname).
-	// The socket will be a server, so it will only be listening.
-	// Let the OS map it to the correct address.
-	addr->sin_addr.s_addr = INADDR_ANY;
+    // Step (2): specify socket address (hostname).
+    // The socket will be a server, so it will only be listening.
+    // Let the OS map it to the correct address.
+    addr->sin_addr.s_addr = INADDR_ANY;
 
-	// Step (3): Set the port value.
-	// If port is 0, the OS will choose the port for us.
-	// Use htons to convert from local byte order to network byte order.
-	addr->sin_port = htons(port);
+    // Step (3): Set the port value.
+    // If port is 0, the OS will choose the port for us.
+    // Use htons to convert from local byte order to network byte order.
+    addr->sin_port = htons(port);
 
-	return 0;
+    return 0;
 }
 
 /**
@@ -48,25 +48,25 @@ int make_server_sockaddr(struct sockaddr_in *addr, int port) {
  *		int err = make_client_sockaddr(&client, "141.88.27.42", 8888);
  */
 int make_client_sockaddr(struct sockaddr_in *addr, const char *hostname, int port) {
-	// Step (1): specify socket family.
-	// This is an internet socket.
-	addr->sin_family = AF_INET;
+    // Step (1): specify socket family.
+    // This is an internet socket.
+    addr->sin_family = AF_INET;
 
-	// Step (2): specify socket address (hostname).
-	// The socket will be a client, so call this unix helper function
-	// to convert a hostname string to a useable `hostent` struct.
-	struct hostent *host = gethostbyname(hostname);
-	if (host == nullptr) {
-		fprintf(stderr, "%s: unknown host\n", hostname);
-		return -1;
-	}
-	memcpy(&(addr->sin_addr), host->h_addr, host->h_length);
+    // Step (2): specify socket address (hostname).
+    // The socket will be a client, so call this unix helper function
+    // to convert a hostname string to a useable `hostent` struct.
+    struct hostent *host = gethostbyname(hostname);
+    if (host == nullptr) {
+        fprintf(stderr, "%s: unknown host\n", hostname);
+        return -1;
+    }
+    memcpy(&(addr->sin_addr), host->h_addr, host->h_length);
 
-	// Step (3): Set the port value.
-	// Use htons to convert from local byte order to network byte order.
-	addr->sin_port = htons(port);
+    // Step (3): Set the port value.
+    // Use htons to convert from local byte order to network byte order.
+    addr->sin_port = htons(port);
 
-	return 0;
+    return 0;
 }
 
 /**
@@ -78,13 +78,13 @@ int make_client_sockaddr(struct sockaddr_in *addr, const char *hostname, int por
  * Returns:
  *		The port number of the socket, or -1 on failure.
  */
- int get_port_number(int sockfd) {
- 	struct sockaddr_in addr;
-	socklen_t length = sizeof(addr);
-	if (getsockname(sockfd, (sockaddr *) &addr, &length) == -1) {
-		perror("Error getting port of socket");
-		return -1;
-	}
-	// Use ntohs to convert from network byte order to host byte order.
-	return ntohs(addr.sin_port);
- }
+int get_port_number(int sockfd) {
+    struct sockaddr_in addr;
+    socklen_t length = sizeof(addr);
+    if (getsockname(sockfd, (sockaddr *)&addr, &length) == -1) {
+        perror("Error getting port of socket");
+        return -1;
+    }
+    // Use ntohs to convert from network byte order to host byte order.
+    return ntohs(addr.sin_port);
+}
--- starter-code/server.cpp
+++ starter-code/server.cpp
@@ -1,10 +1,10 @@
-#include <arpa/inet.h>		// htons()
-#include <stdio.h>		// printf(), perror()
-#include <stdlib.h>		// atoi()
-#include <sys/socket.h>		// socket(), bind(), listen(), accept(), send(), recv()
-#include <unistd.h>		// close()
+#include <arpa/inet.h>   // htons()
+#include <stdio.h>       // printf(), perror()
+#include <stdlib.h>      // atoi()
+#include <sys/socket.h>  // socket(), bind(), listen(), accept(), send(), recv()
+#include <unistd.h>      // close()
 
-#include "helpers.h"		// make_server_sockaddr(), get_port_number()
+#include "helpers.h"  // make_server_sockaddr(), get_port_number()
 
 static const size_t MAX_MESSAGE_SIZE = 256;
 
@@ -18,16 +18,15 @@ static const size_t MAX_MESSAGE_SIZE = 256;
  *		0 on success, -1 on failure.
  */
 int handle_connection(int connectionfd) {
+    printf("New connection %d\n", connectionfd);
 
-	printf("New connection %d\n", connectionfd);
+    // (1) Receive message from client.
 
-	// (1) Receive message from client.
+    // (2) Print out the message
 
-	// (2) Print out the message
+    // (3) Close connection
 
-	// (3) Close connection
-
-	return 0;
+    return 0;
 }
 
 /**
@@ -41,38 +40,37 @@ int handle_connection(int connectionfd) {
  *		-1 on failure, does not return on success.
  */
 int run_server(int port, int queue_size) {
+    // (1) Create socket
 
-	// (1) Create socket
+    // (2) Set the "reuse port" socket option
 
-	// (2) Set the "reuse port" socket option
+    // (3) Create a sockaddr_in struct for the proper port and bind() to it.
+    struct sockaddr_in addr;
+    if (make_server_sockaddr(&addr, port) == -1) {
+        return -1;
+    }
 
-	// (3) Create a sockaddr_in struct for the proper port and bind() to it.
-	struct sockaddr_in addr;
-	if (make_server_sockaddr(&addr, port) == -1) {
-		return -1;
-	}
+    // (3b) Bind to the port.
 
-	// (3b) Bind to the port.
+    // (3c) Detect which port was chosen.
+    port = get_port_number(sockfd);
+    printf("Server listening on port %d...\n", port);
 
-	// (3c) Detect which port was chosen.
-	port = get_port_number(sockfd);
-	printf("Server listening on port %d...\n", port);
+    // (4) Begin listening for incoming connections.
 
-	// (4) Begin listening for incoming connections.
-
-	// (5) Serve incoming connections one by one forever.
+    // (5) Serve incoming connections one by one forever.
 }
 
 int main(int argc, const char **argv) {
-	// Parse command line arguments
-	if (argc != 2) {
-		printf("Usage: ./server port_num\n");
-		return 1;
-	}
-	int port = atoi(argv[1]);
+    // Parse command line arguments
+    if (argc != 2) {
+        printf("Usage: ./server port_num\n");
+        return 1;
+    }
+    int port = atoi(argv[1]);
 
-	if (run_server(port, 10) == -1) {
-		return 1;
-	}
-	return 0;
+    if (run_server(port, 10) == -1) {
+        return 1;
+    }
+    return 0;
 }
